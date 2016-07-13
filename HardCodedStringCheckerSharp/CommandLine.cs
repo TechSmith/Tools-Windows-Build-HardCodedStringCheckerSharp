f60e0e7ded281816eebf8443ecf29d6afd0eb736
A.J. Orians <ajorians@gmail.com>
2016-07-12 20:53:00 -0400

    Moved some variables into a command line class.

---
+++ HardCodedStringCheckerSharp/CommandLine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HardCodedStringCheckerSharp
+{
+   public enum Action
+   {
+      ReportHCS,
+      FixHCS
+   }
+   public class CommandLine
+   {
+      public string ReposityPath { get; private set; } = String.Empty;
+      public Action Action { get; private set; } = Action.ReportHCS;
+      public bool FailBuildWithHSC { get; private set; } = false;
+
+      public CommandLine()
+      {
+
+      }
+
+      public bool Parse( string[] args )
+      {
+         if ( args.Count() < 2 )
+         {
+            Console.WriteLine( "Usage: <Program> RepoDirectory (Report or Fix) (--FailOnHCS optional)" );
+            return false;
+         }
+
+         ReposityPath = args[0];
+
+         //------------------------------
+         if ( args[1] == "Fix" )
+            Action = Action.FixHCS;
+
+         //------------------------------
+
+         if ( args.Count() >= 3 && args[2] == "--FailOnHCS" )
+            FailBuildWithHSC = true;
+
+         return true;
+      }
+   }
+}
