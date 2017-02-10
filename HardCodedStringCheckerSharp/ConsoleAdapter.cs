23467a27f158044d1796d637edecddc1c9b481fa
Alex Novak <alexwnovak@gmail.com>
2017-02-10 16:54:58 -0500

    +IConsole blank interface and concrete
    Same thing here--wraps the Console static class.

---
+++ HardCodedStringCheckerSharp/ConsoleAdapter.cs
@@ -0,0 +1,6 @@
+namespace HardCodedStringCheckerSharp
+{
+   public class ConsoleAdapter : IConsole
+   {
+   }
+}
---
+++ HardCodedStringCheckerSharp/IConsole.cs
@@ -0,0 +1,6 @@
+﻿namespace HardCodedStringCheckerSharp
+{
+   public interface IConsole
+   {
+   }
+}
