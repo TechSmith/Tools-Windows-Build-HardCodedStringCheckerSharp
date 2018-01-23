5982226d0de814e3af89226ca640661dc8a50331
Travis Chapman <traviswchapman@gmail.com>
2018-01-23 14:01:58 -0500

    Add AppControllerTests file

---
+++ HardCodedStringCheckerSharp.UnitTests/AppControllerTest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace HardCodedStringCheckerSharp.UnitTests
+{
+   [TestClass]
+   public class AppControllerTest
+   {
+   }
+}
