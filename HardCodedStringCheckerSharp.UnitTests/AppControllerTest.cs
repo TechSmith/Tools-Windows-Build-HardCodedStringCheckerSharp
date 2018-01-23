ec177b10009f81272e706449125079a1961d6c52
Travis Chapman <traviswchapman@gmail.com>
2018-01-23 14:02:37 -0500

    Default encoding if byte order mark is not there

--- HardCodedStringCheckerSharp.UnitTests/AppControllerTest.cs
+++ HardCodedStringCheckerSharp.UnitTests/AppControllerTest.cs
@@ -5,5 +5,12 @@ namespace HardCodedStringCheckerSharp.UnitTests
    [TestClass]
    public class AppControllerTest
    {
+      [TestMethod]
+      public void GetEncoding_NoByteOrderMarkerReturn_DefaultsToWindows1252()
+      {
+         var appController = new AppController( Mock.Of<IFileSystem>(), Mock.Of<IConsole>() );
+
+         appController.GetEncoding( It.IsAny<string>() ).Should().Be( Encoding.GetEncoding( "Windows-1252" ) );
+      }
    }
 }
