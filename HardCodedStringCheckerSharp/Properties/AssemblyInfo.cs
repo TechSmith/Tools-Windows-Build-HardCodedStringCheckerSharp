b44db4b8a86eb14c526dc5c4501fb9e8ab90026a
Travis Chapman <traviswchapman@gmail.com>
2018-01-23 14:01:12 -0500

    Expose internals to the test project

--- HardCodedStringCheckerSharp/Properties/AssemblyInfo.cs
+++ HardCodedStringCheckerSharp/Properties/AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright( "Copyright © 2016-2017 TechSmith Corporation. All rights reserved." )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
+[assembly: InternalsVisibleTo( "HardCodedStringCheckerSharp.UnitTests" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
