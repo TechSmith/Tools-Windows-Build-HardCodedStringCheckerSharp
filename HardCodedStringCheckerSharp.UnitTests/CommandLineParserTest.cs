ce68b3bb0c8075f4b68696f94b85e42c184856a4
Dave O'Rourke <d.orourke@techsmith.com>
2018-02-27 14:17:26 -0500

    Added CommandLineParser and tests

---
+++ HardCodedStringCheckerSharp.UnitTests/CommandLineParserTest.cs
@@ -0,0 +1,166 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HardCodedStringCheckerSharp.UnitTests
+{
+   [TestClass]
+   public class CommandLineParserTest
+   {
+      [TestMethod]
+      public void ParseCommandLine_ZeroArgs_ReturnsNull()
+      {
+         var parser = new CommandLineParser();
+
+         string[] args = new string[0];
+         var opts = parser.ParseCommandLine( args );
+
+         opts.Should().BeNull();
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_OneArg_ReturnsNull()
+      {
+         var parser = new CommandLineParser();
+
+         string[] args = { @"C:\RepoRoot" };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.Should().BeNull();
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_SecondArgIsInvalid_ReturnsNull()
+      {
+         var parser = new CommandLineParser();
+
+         string[] args = { @"C:\RepoRoot", "Invalid" };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.Should().BeNull();
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_RepoDirectorySpecified_RepoDirectoryIsSetCorrectly()
+      {
+         var parser = new CommandLineParser();
+
+         const string repoRoot = @"C:\RepoRoot";
+         string[] args = { repoRoot, "Report" };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.RepoDirectory.Should().Be( repoRoot );
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_SecondArgIsFix_ActionIsFixHCS()
+      {
+         var parser = new CommandLineParser();
+
+         string[] args = { @"C:\RepoRoot", "Fix" };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.Action.Should().Be( Action.FixHCS );
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_SecondArgIsReport_ActionIsReportHCS()
+      {
+         var parser = new CommandLineParser();
+
+         string[] args = { @"C:\RepoRoot", "Report" };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.Action.Should().Be( Action.ReportHCS );
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_FailOnHCSNotSpecified_SetsFailOnHCSToFalse()
+      {
+         var parser = new CommandLineParser();
+
+         string[] args = { @"C:\RepoRoot", "Report" };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.FailOnHCS.Should().BeFalse();
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_ThirdArgIsInvalid_ReturnsNull()
+      {
+         var parser = new CommandLineParser();
+
+         string[] args = { @"C:\RepoRoot", "Report", "Invalid" };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.Should().BeNull();
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_ThirdArgIsFailOnHCS_SetsFailOnHCSToTrue()
+      {
+         var parser = new CommandLineParser();
+
+         string[] args = { @"C:\RepoRoot", "Report", "--FailOnHCS" };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.FailOnHCS.Should().BeTrue();
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_ThirdArgIsExcludeButMissingExcludeFile_ReturnsNull()
+      {
+         var parser = new CommandLineParser();
+
+         string[] args = { @"C:\RepoRoot", "Report", "--Exclude" };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.Should().BeNull();
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_ThirdArgIsExcludeWithFourthArgExcludeFile_ReturnsExcludeFile()
+      {
+         var parser = new CommandLineParser();
+
+         string excludeFile = @"C:\Path\To\ExcludeFile.txt";
+         string[] args = { @"C:\RepoRoot", "Report", "--Exclude", excludeFile };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.ExcludeFile.Should().Be( excludeFile );
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_FourthArgIsInvalid_ReturnsNull()
+      {
+         var parser = new CommandLineParser();
+
+         string[] args = { @"C:\RepoRoot", "Report", "--FailOnHCS", "Invalid" };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.Should().BeNull();
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_FourthArgIsExcludeButMissingExcludeFile_ReturnsNull()
+      {
+         var parser = new CommandLineParser();
+
+         string[] args = { @"C:\RepoRoot", "Report", "--FailOnHCS", "--Exclude" };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.Should().BeNull();
+      }
+
+      [TestMethod]
+      public void ParseCommandLine_FourthArgIsExcludeWithFifthArgExcludeFile_ReturnsExcludeFile()
+      {
+         var parser = new CommandLineParser();
+
+         string excludeFile = @"C:\Path\To\ExcludeFile.txt";
+         string[] args = { @"C:\RepoRoot", "Report", "--FailOnHCS", "--Exclude", excludeFile };
+         var opts = parser.ParseCommandLine( args );
+
+         opts.ExcludeFile.Should().Be( excludeFile );
+      }
+   }
+}
---
+++ HardCodedStringCheckerSharp/CommandLineParser.cs
@@ -0,0 +1,63 @@
+﻿namespace HardCodedStringCheckerSharp
+{
+   class CommandLineParser : ICommandLineParser
+   {
+      public CommandLineOptions ParseCommandLine( string[] args )
+      {
+         // Expecting between 2 and 5 arguments
+         if ( args.Length < 2 || args.Length > 5 )
+         {
+            return null;
+         }
+
+         var opts = new CommandLineOptions();
+
+         // The first argument is assumed to be the repo directory
+         opts.RepoDirectory = args[0];
+
+         // The second argument must be "Fix" or "Report"
+         string secondArg = args[1];
+         if ( secondArg == "Fix" )
+         {
+            opts.Action = Action.FixHCS;
+         }
+         else if ( secondArg == "Report" )
+         {
+            opts.Action = Action.ReportHCS;
+         }
+         else
+         {
+            return null;
+         }
+
+         if ( args.Length >= 3 )
+         {
+            // Start at third arg (index 2) because we handled first and second args above
+            for ( int i = 2; i < args.Length; i++ )
+            {
+               if ( args[i] == "--FailOnHCS" )
+               {
+                  opts.FailOnHCS = true;
+               }
+               else if ( args[i] == "--Exclude" )
+               {
+                  // The exclude file is the next argument
+                  i = i + 1;
+                  if ( i >= args.Length )
+                  {
+                     return null;  // not enough arguments
+                  }
+                  opts.ExcludeFile = args[i];
+               }
+               else
+               {
+                  // Invalid argument encountered
+                  return null;
+               }
+            }
+         }
+
+         return opts;
+      }
+   }
+}
