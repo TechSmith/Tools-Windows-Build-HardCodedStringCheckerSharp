0b3a682e50e66d600ed15a59ce84ae74d8927abc
Dave O'Rourke <d.orourke@techsmith.com>
2018-02-28 11:07:16 -0500

    Added ExcludeFileParser and tests

---
+++ HardCodedStringCheckerSharp.UnitTests/ExcludeFileParserTest.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace HardCodedStringCheckerSharp.UnitTests
+{
+   [TestClass]
+   public class ExcludeFileParserTest
+   {
+      [TestMethod]
+      public void ParseExcludeFile_FileHasCommentsAndWhitespace_ReturnsCorrectResults()
+      {
+         // Arrange
+         string folder1 = @"\Folder1\To\Exclude";
+         string folder2 = @"\Folder2\To\Exclude";
+
+         string[] excludeFileContents = new string[]
+         {
+            $"// Exclude file for C# Hard Coded String Checker",
+            $"//",
+            $"// The following directories will be excluded: ",
+            $"    ",
+            $"{folder1}",
+            $"{folder2}  // in-line comment",
+            $"  "
+         };
+
+         var mockFileSystem = new Mock<IFileSystem>();
+         mockFileSystem.Setup( mfs => mfs.ReadAllLines( It.IsAny<string>() ) ).Returns( excludeFileContents );
+
+         // Act
+         var parser = new ExcludeFileParser( mockFileSystem.Object );
+         List<string> exclusions = parser.ParseExcludeFile( "DoesNotMatter" );
+
+         // Assert
+         exclusions.Count.Should().Be( 2 );
+         exclusions[0].Should().Be( folder1 );
+         exclusions[1].Should().Be( folder2 );
+      }
+
+      [TestMethod]
+      public void PrependRepoRootDir_ExclusionsHaveLeadingBackslash_CombinesPathsCorrectly()
+      {
+         // Arrange
+         string repoRoot = @"C:\RepoRoot\";
+         var exclusions = new List<string>
+         {
+            @"\Folder1\To\Exclude",  // The leading backslash changes the behavior of Path.Combine
+            @"\Folder2\To\Exclude"
+         };
+         var expectedResult = new List<string>
+         {
+            @"C:\RepoRoot\Folder1\To\Exclude",
+            @"C:\RepoRoot\Folder2\To\Exclude"
+         };
+
+         // Act
+         var parser = new ExcludeFileParser( Mock.Of<IFileSystem>() );
+         var result = parser.PrependRepoRootDir( repoRoot, exclusions );
+
+        // Assert
+         result.Should().Equal( expectedResult );
+      }
+
+      [TestMethod]
+      public void PrependRepoRootDir_RepoRootAndExclusionsDoNotHaveLeadingBackslash_CombinesPathsCorrectly()
+      {
+         // Arrange
+         string repoRoot = @"C:\RepoRoot";
+         var exclusions = new List<string>
+         {
+            @"Folder1\To\Exclude",
+            @"Folder2\To\Exclude"
+         };
+         var expectedResult = new List<string>
+         {
+            @"C:\RepoRoot\Folder1\To\Exclude",
+            @"C:\RepoRoot\Folder2\To\Exclude"
+         };
+
+         // Act
+         var parser = new ExcludeFileParser( Mock.Of<IFileSystem>() );
+         var result = parser.PrependRepoRootDir( repoRoot, exclusions );
+
+         // Assert
+         result.Should().Equal( expectedResult );
+      }
+   }
+}
---
+++ HardCodedStringCheckerSharp/ExcludeFileParser.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HardCodedStringCheckerSharp
+{
+   public class ExcludeFileParser : IExcludeFileParser
+   {
+      private readonly IFileSystem _fileSystem;
+
+      public ExcludeFileParser( IFileSystem fileSystem )
+      {
+         _fileSystem = fileSystem;
+      }
+
+      public List<string> ParseExcludeFile( string excludeFile )
+      {
+         var exclusions = new List<string>();
+
+         string[] lines = _fileSystem.ReadAllLines( excludeFile );
+         foreach( string line in lines )
+         {
+            // Strip any comments
+            string lineMinusComments = line;
+            int commentStart = line.IndexOf( "//" );
+            if ( commentStart >= 0 )
+            {
+               lineMinusComments = line.Substring( 0, commentStart );
+            }
+
+            // If there's anything left after trimming whitespace, add it as an exclusion
+            string trimmedLine = lineMinusComments.Trim();
+            if ( !string.IsNullOrWhiteSpace( trimmedLine ) )
+            {
+               exclusions.Add( trimmedLine );
+            }
+         }
+
+         return exclusions;
+      }
+
+      public List<string> PrependRepoRootDir( string repoRootDir, List<string> exclusions )
+      {
+         // Note: The Trim of the backslash is necessary because a leading backslash
+         //       on the second argument causes Path.Combine to think the second path
+         //       is a root path, and it doesn't actually combine the strings. -dro
+         return exclusions.Select( ex => Path.Combine( repoRootDir, ex.Trim('\\') ) ).ToList();
+      }
+   }
+}
