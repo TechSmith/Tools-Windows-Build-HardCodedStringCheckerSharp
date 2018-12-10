7d6c05029a5ed5477fa6772b42cbd318ecf5e099
Alex Novak <alexwnovak@gmail.com>
2018-12-10 18:52:27 -0500

    Initializing inline to ditch the redundant default constructor

--- HardCodedStringCheckerSharp/CommandLineOptions.cs
+++ HardCodedStringCheckerSharp/CommandLineOptions.cs
@@ -5,20 +5,10 @@ namespace HardCodedStringCheckerSharp
 {
    public class CommandLineOptions
    {
-      public CommandLineOptions()
-      {
-         // Default values
-         RepoDirectory = string.Empty;
-         Action = Action.ReportHCS;
-         FailOnHCS = false;
-         ExcludeFile = string.Empty;
-         SpecificFiles = Enumerable.Empty<string>();
-      }
-
-      public string RepoDirectory { get; set; }
-      public Action Action { get; set; }
-      public bool FailOnHCS { get; set; }
-      public string ExcludeFile { get; set; }
-      public IEnumerable<string> SpecificFiles { get; set; }
+      public string RepoDirectory { get; set; } = string.Empty;
+      public Action Action { get; set; } = Action.ReportHCS;
+      public bool FailOnHCS { get; set; } = false;
+      public string ExcludeFile { get; set; } = string.Empty;
+      public IEnumerable<string> SpecificFiles { get; set; } = Enumerable.Empty<string>();
    }
 }
